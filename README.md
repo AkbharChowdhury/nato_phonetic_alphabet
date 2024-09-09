4e0da363d2d0d79f5516de4f226ba8427fa1b24c
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-09-09 21:08:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nato_phonetic_alphabet
+ this application finds the list of names from a given letter
---
+++ nato_phonetic_alphabet.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import itertools
+
+def format_list(l):
+    return f'{", ".join(l[:-1])} and {l[-1]}' if len(l) > 1 else l[0]
+def main():
+    try:
+        data = pd.read_csv("nato_phonetic_alphabet.csv")
+        phonetics = {row.letter: list() for (_, row) in data.iterrows()}
+        for (_, row) in data.iterrows():
+            phonetics[row['letter']].append(row['code'])
+        print(phonetics)
+        word = input("Enter a word: ").upper()
+        result = list(itertools.chain(*[phonetics[word] for _ in word]))
+        print(format_list(result))
+    except KeyError:
+        print(f"{word} is not in the dictionary.")
+
+
+if __name__ == '__main__':
+    main()
